e1329513da99313de57f1531d802c6a9a4838a84
Chris Dziemborowicz <chris@dziemborowicz.com>
2014-12-10 23:32:18 -0800

    Add VisualTreeUtility

---
+++ Hourglass/VisualTreeUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Hourglass
+{
+    public class VisualTreeUtility
+    {
+        public static IEnumerable<T> GetVisualChildren<T>(DependencyObject depObj)
+            where T : DependencyObject
+        {
+            if (depObj != null)
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                        yield return (T)child;
+
+                    foreach (T childOfChild in GetVisualChildren<T>(child))
+                        yield return childOfChild;
+                }
+        }
+    }
+}
