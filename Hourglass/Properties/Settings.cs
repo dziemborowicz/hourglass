1037bd09e89ac4c735396b09000b593218dc0d9a
Chris Dziemborowicz <chris@dziemborowicz.com>
2015-05-10 02:55:28 -0700

    Resolve #15: Create portable version of the application

---
+++ Hourglass/Properties/Settings.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Settings.cs" company="Chris Dziemborowicz">
+//   Copyright (c) Chris Dziemborowicz. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Hourglass.Properties
+{
+    using System.Configuration;
+
+    /// <summary>
+    /// Application settings.
+    /// </summary>
+#if PORTABLE
+    [SettingsProvider(typeof(PortableSettingsProvider))]
+#endif
+    internal sealed partial class Settings 
+    {
+    }
+}
