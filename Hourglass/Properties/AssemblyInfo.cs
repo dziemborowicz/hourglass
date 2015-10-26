d1eccca804d9a3e7848ae97285671854c4dc0746
Chris Dziemborowicz <chris@dziemborowicz.com>
2015-10-25 20:06:53 -0700

    Fix up AssemblyInfo.cs files

--- Hourglass/Properties/AssemblyInfo.cs
+++ Hourglass/Properties/AssemblyInfo.cs
@@ -20,6 +20,6 @@ using System.Windows;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
-[assembly: GuidAttribute("83DBAA61-6193-4288-BBB7-BEAEC33FE321")]
+[assembly: Guid("83DBAA61-6193-4288-BBB7-BEAEC33FE321")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: ComVisible(false)]
