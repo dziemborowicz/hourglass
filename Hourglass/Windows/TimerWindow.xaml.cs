dziemborowicz/hourglass

commit c874899b90751a9459a5661275894073255aacd7

Resolve #97: Add option to remove Window frame/title bar

Hourglass/Windows/TimerWindow.xaml.cs
@@ -364,21 +364,7 @@ namespace Hourglass.Windows
                 }
 
                 this.isFullScreen = value;
-
-                if (this.isFullScreen)
-                {
-                    this.WindowStyle = WindowStyle.None;
-                    this.WindowState = WindowState.Normal; // Needed to put the window on top of the taskbar
-                    this.WindowState = WindowState.Maximized;
-                    this.ResizeMode = ResizeMode.NoResize;
-                }
-                else
-                {
-                    this.WindowStyle = WindowStyle.SingleBorderWindow;
-                    this.WindowState = this.restoreWindowState;
-                    this.ResizeMode = ResizeMode.CanResize;
-                }
-
+                this.UpdateWindowStyle();
                 this.OnPropertyChanged("IsFullScreen");
             }
         }
@@ -1048,6 +1034,7 @@ namespace Hourglass.Windows
                     this.UpdateBoundTheme();
                     this.UpdateKeepAwake();
                     this.UpdateWindowTitle();
+                    this.UpdateWindowStyle();
                     return;
 
                 case TimerWindowMode.Status:
@@ -1103,6 +1090,7 @@ namespace Hourglass.Windows
                     this.UpdateBoundTheme();
                     this.UpdateKeepAwake();
                     this.UpdateWindowTitle();
+                    this.UpdateWindowStyle();
                     return;
             }
         }
@@ -1114,7 +1102,7 @@ namespace Hourglass.Windows
         {
             this.InnerGrid.Background = this.Theme.BackgroundBrush;
             this.ProgressBar.Foreground = this.Theme.ProgressBarBrush;
-            this.ProgressBar.Background  = this.Theme.ProgressBackgroundBrush;
+            this.ProgressBar.Background = this.Theme.ProgressBackgroundBrush;
             this.InnerNotificationBorder.BorderBrush = this.Theme.ExpirationFlashBrush;
             this.OuterNotificationBorder.Background = this.Theme.ExpirationFlashBrush;
             this.TimerTextBox.Foreground = this.Theme.PrimaryTextBrush;
@@ -1207,6 +1195,11 @@ namespace Hourglass.Windows
         {
             switch (this.Options.WindowTitleMode)
             {
+                case WindowTitleMode.None:
+                    // Although the title bar is hidden in this mode, the window title is still used for the Taskbar.
+                    this.Title = Properties.Resources.TimerWindowTitle;
+                    break;
+
                 case WindowTitleMode.ApplicationName:
                     this.Title = Properties.Resources.TimerWindowTitle;
                     break;
@@ -1320,6 +1313,33 @@ namespace Hourglass.Windows
                     }
 
                     break;
+            }
+        }
+
+        /// <summary>
+        /// Updates the window style.
+        /// </summary>
+        private void UpdateWindowStyle()
+        {
+            if (this.isFullScreen)
+            {
+                this.WindowStyle = WindowStyle.None;
+
+                if (this.WindowState != WindowState.Maximized)
+                {
+                    this.WindowState = WindowState.Normal; // Needed to put the window on top of the taskbar
+                    this.WindowState = WindowState.Maximized;
+                }
+
+                this.ResizeMode = ResizeMode.NoResize;
+            }
+            else
+            {
+                this.WindowStyle = this.Options.WindowTitleMode == WindowTitleMode.None
+                    ? WindowStyle.None
+                    : WindowStyle.SingleBorderWindow;
+                this.WindowState = this.restoreWindowState;
+                this.ResizeMode = ResizeMode.CanResize;
             }
         }
 
@@ -1823,6 +1843,11 @@ namespace Hourglass.Windows
         /// <param name="e">The event data.</param>
         private void WindowMouseDown(object sender, MouseButtonEventArgs e)
         {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+
             if (e.OriginalSource is Panel)
             {
                 this.CancelOrReset();

Hourglass/CommandLineArguments.cs
@@ -866,6 +866,9 @@ namespace Hourglass
 
             switch (value.ToLowerInvariant())
             {
+                case "none":
+                    return WindowTitleMode.None;
+
                 case "app":
                     return WindowTitleMode.ApplicationName;
 

Hourglass/Properties/Resources.Designer.cs
@@ -305,6 +305,15 @@ namespace Hourglass.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to _None.
+        /// </summary>
+        internal static string ContextMenuNoWindowTitleMenuItem {
+            get {
+                return ResourceManager.GetString("ContextMenuNoWindowTitleMenuItem", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to _Open all saved timers.
         /// </summary>
         internal static string ContextMenuOpenAllSavedTimersMenuItem {

Hourglass/Properties/Resources.resx
@@ -1181,6 +1181,10 @@ $</value>
     <value>Are you sure you want to delete the selected theme?</value>
     <comment>The prompt displayed when the user deletes a theme</comment>
   </data>
+  <data name="ContextMenuNoWindowTitleMenuItem" xml:space="preserve">
+    <value>_None</value>
+    <comment>The text for the application name menu item, where the character following the optional underscore (_) is the access key</comment>
+  </data>
   <data name="ContextMenuApplicationNameWindowTitleMenuItem" xml:space="preserve">
     <value>_Application name</value>
     <comment>The text for the application name menu item, where the character following the optional underscore (_) is the access key</comment>

Hourglass/Timing/TimerOptions.cs
@@ -19,6 +19,11 @@ namespace Hourglass.Timing
     public enum WindowTitleMode
     {
         /// <summary>
+        /// Hides the timer window title bar.
+        /// </summary>
+        None,
+
+        /// <summary>
         /// The timer window title is set to show the application name.
         /// </summary>
         ApplicationName,

Hourglass/Windows/ContextMenu.cs
@@ -177,6 +177,11 @@ namespace Hourglass.Windows
         private MenuItem windowTitleMenuItem;
 
         /// <summary>
+        /// The "None" window title <see cref="MenuItem"/>.
+        /// </summary>
+        private MenuItem noWindowTitleMenuItem;
+
+        /// <summary>
         /// The "Application name" window title <see cref="MenuItem"/>.
         /// </summary>
         private MenuItem applicationNameWindowTitleMenuItem;
@@ -668,6 +673,18 @@ namespace Hourglass.Windows
             this.windowTitleMenuItem = new MenuItem();
             this.windowTitleMenuItem.Header = Properties.Resources.ContextMenuWindowTitleMenuItem;
             this.advancedOptionsMenuItem.Items.Add(this.windowTitleMenuItem);
+
+            // No window title
+            this.noWindowTitleMenuItem = new MenuItem();
+            this.noWindowTitleMenuItem.Header = Properties.Resources.ContextMenuNoWindowTitleMenuItem;
+            this.noWindowTitleMenuItem.IsCheckable = true;
+            this.noWindowTitleMenuItem.Tag = WindowTitleMode.None;
+            this.noWindowTitleMenuItem.Click += this.WindowTitleMenuItemClick;
+            this.noWindowTitleMenuItem.Click += this.CheckableMenuItemClick;
+            this.windowTitleMenuItem.Items.Add(this.noWindowTitleMenuItem);
+            this.selectableWindowTitleMenuItems.Add(this.noWindowTitleMenuItem);
+
+            this.windowTitleMenuItem.Items.Add(new Separator());
 
             // Application name (window title)
             this.applicationNameWindowTitleMenuItem = new MenuItem();

commit f17dc055a5e3a62bd28c25487ea1e439597e1212

Resolve #187: Fix minimize button not working
This bug was introduced in c874899b90751a9459a5661275894073255aacd7. Specifically, the new UpdateWindowStyle method unconditionally set the WindowState to the saved restoreWindowState, including when the window was minimized. This method was called frequently (including on timer ticks) and would constantly un-minimize the timer window.

This change resolves the issue by not clobbering the minimize WindowState when it is set in UpdateWindowStyle.

Hourglass/Windows/TimerWindow.xaml.cs
@@ -1370,29 +1370,36 @@ namespace Hourglass.Windows
 
                 WindowChrome.SetWindowChrome(this, null);
             }
-            else if (this.Options.WindowTitleMode == WindowTitleMode.None)
-            {
-                this.WindowStyle = WindowStyle.SingleBorderWindow;
-                this.WindowState = this.restoreWindowState;
-                this.ResizeMode = ResizeMode.CanResize;
-
-                if (WindowChrome.GetWindowChrome(this)?.CaptionHeight != 0)
-                {
-                    WindowChrome.SetWindowChrome(
-                        this,
-                        new WindowChrome
-                        {
-                            CaptionHeight = 0
-                        });
-                }
-            }
             else
             {
                 this.WindowStyle = WindowStyle.SingleBorderWindow;
-                this.WindowState = this.restoreWindowState;
+
+                if (this.WindowState != WindowState.Minimized)
+                {
+                    this.WindowState = this.restoreWindowState;
+                }
+
                 this.ResizeMode = ResizeMode.CanResize;
 
-                WindowChrome.SetWindowChrome(this, null);
+                if (this.Options.WindowTitleMode == WindowTitleMode.None)
+                {
+                    if (WindowChrome.GetWindowChrome(this)?.CaptionHeight != 0)
+                    {
+                        WindowChrome.SetWindowChrome(
+                            this,
+                            new WindowChrome
+                            {
+                                CaptionHeight = 0
+                            });
+                    }
+                }
+                else
+                {
+                    if (WindowChrome.GetWindowChrome(this) != null)
+                    {
+                        WindowChrome.SetWindowChrome(this, null);
+                    }
+                }
             }
         }
 